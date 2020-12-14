61addc1ecaf88bb9efbd94abf3b3a46f04d33f2b
Dmitry Diokin <dmitry.dyokin@gmail.com>
2020-12-14 23:35:05 +0300

    Add initial project setup & dependencies

---
+++ src/main/java/com/freemmy/restapidesignfirst/RestApiDesignFirstApplication.java
@@ -0,0 +1,13 @@
+package com.freemmy.restapidesignfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDesignFirstApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestApiDesignFirstApplication.class, args);
+    }
+}
---
+++ src/test/java/com/freemmy/restapidesignfirst/RestApiDesignFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freemmy.restapidesignfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDesignFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
